701ff50f7203efcaff18c7d669c3e2494a9060b0
Donovan Gervais <applemac@Donovans-MacBook-Pro-7.local>
2020-12-03 12:08:56 +0000

    Correcte DB provision

--- App_Multi_Virtual_Machine/environment/db/provision.sh
+++ App_Multi_Virtual_Machine/environment/db/provision.sh
@@ -1,30 +1,28 @@
-# Installing mongodb 3.2.20 - Do it within VM
-# Generate keys for mongo db
+#!/bin/bash
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
-echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+# Update and upgrade any packages
+sudo apt-get update
+sudo apt-get upgrade
 
-# Installs the 3.2.20 version of mongoDB and starts
-sudo apt update -y
+# import a public GPG Key for MongoDB specifiying the version
+wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
+
+# Create 3.2 list file for Ubuntu 18.04 (Bionic. HOWEVER: )
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+sudo apt-get update
+
 sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
 
-# replace the original .conf file with the one that forces 0.0.0.0 port
-sudo rm /etc/mongod.conf
-sudo ln -s /home/ubuntu/environment/mongod.conf /etc/mongod.conf
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
 
+sudo sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
-# Can also -> sudo cp -f mongod.conf /etc/mongod.conf <- which overwrites the mongod.conf file
-
-
-# if mongo is is set up correctly these will run it
-sudo systemctl restart mongod
-sudo systemctl enable mongod.service --now
-
-
-
-# Can also do the below which is much faster and doesnt require writing .conf files
-# sudo nano /etc/mongod.conf
-# sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-# sudo service mongod restart
+sudo systemctl start mongod
+sudo systemctl enable mongod
 
 
